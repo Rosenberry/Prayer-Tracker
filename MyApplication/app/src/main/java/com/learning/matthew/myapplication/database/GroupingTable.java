733bacc3f9f811c2b8bdb310b200edb3b152c0e9
Matthew Rosenbaum <mat.g.rosen@gmail.com>
2015-01-19 11:02:28 -0600

    reworked delete button

--- MyApplication/app/src/main/java/com/learning/matthew/myapplication/database/GroupingTable.java
+++ MyApplication/app/src/main/java/com/learning/matthew/myapplication/database/GroupingTable.java
@@ -13,13 +13,13 @@ public class GroupingTable {
     protected static final String COMMA_SEP = ", ";
 
     // column name and data type
-    protected static final String GROUPING_ID = "Grouping_ID";
+    protected static final String _ID = "Grouping_ID";
     protected static final String COLUMN_GROUP_NAME = "groupName";
     protected static final String TEXT = " TEXT";
 
     public static final String CREATE_SQL_ENTREES =
             CREATE_TABLE + TABLE_NAME + " (" +
-                    GROUPING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
+                    _ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
                     COLUMN_GROUP_NAME + TEXT + ")";
 
     public static final String SQL_DELETE_ENTRIES =
